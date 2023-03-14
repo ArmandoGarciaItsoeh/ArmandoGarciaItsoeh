df59f89daf0a1249eb5fe7558abb8b585eaaf6f1
ArmandoGarciaItsoeh <122396624+ArmandoGarciaItsoeh@users.noreply.github.com>
2023-03-14 17:34:00 -0600

    Update interactive_lineal_regresion.py

--- interactive_lineal_regresion.py
+++ interactive_lineal_regresion.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 xdata = np.array( [2.1,2.7,2.8,3.2,3.2,4.5,4.6,7.2,8.3,8.9,10,11.5,11.7,12.8,13.8,14.6,14.9] )
 ydata = np.array( [2.94,4.32,1.12,0.64,1.92,3.6,8.28,1.44,9.96,3.56,20,11.5,11.7,17.92,22.08,23.36,17.88] )
 
+data = np.column_stack((xdata, ydata))
+df = pd.DataFrame(data, columns = ['x','y'])
+
+df_pane = pn.pane.DataFrame(df, width=400)
+
 x = np.linspace(xdata.min()-2, xdata.max()+2, 20)
 b = 0
 m = 0
@@ -46,3 +51,5 @@ pn.Row(sliderM, binderM).servable(target='simple_app');
 
 binderB = pn.bind(callbackB, m=sliderM, b=sliderB)
 pn.Row(sliderB, binderB).servable(target='simple_app');
+
+pn.Row(df_pane).servable(target='simple_app');
